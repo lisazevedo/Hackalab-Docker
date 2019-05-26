e76c01d050aa60d4496b31a69e8c33017b935f70
Lis Azevedo <lisazevedo3@gmail.com>
2019-05-25 23:19:20 -0300

    update and rename docker_run.sh

---
+++ docker_run.sh
@@ -0,0 +1,1 @@
+docker run -ti --net=host --privileged -v /home/mcostache/Ezuce:/root -w /root -e DISPLAY=$DISPLAY hackalab/docker
--- dockerrun.sh
+++
@@ -1,1 +0,0 @@
-docker run -ti --net=host --privileged -v /home/mcostache/Ezuce:/root -w /root -e DISPLAY=$DISPLAY hacklab/docker
